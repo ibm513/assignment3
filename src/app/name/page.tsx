8c8c7394f1ab4d702aff2730c37fb11dd12b30f6
Ibrahim Khalid <ibmchemengg@gmail.com>
2024-08-12 17:48:58 +0500

    Assignment3

---
+++ src/app/name/page.tsx
@@ -0,0 +1,28 @@
+
+
+import React from 'react'
+import Link from "next/link"
+
+
+
+
+
+function namepage() {
+
+ 
+  return (
+    <div>
+    <div>
+        This is name page
+    </div>
+
+   <Link href = "/">
+   <button> Back to home page</button>
+    </Link>
+    
+    </div>
+  
+  )
+}
+
+export default namepage
